31fa85bd9ba51e5e30941f55cdbfd835bddf02d2
Zarkonnen <david.stark@zarkonnen.com>
2015-08-24 21:14:52 +0200

    Wobbly tile boundaries.

--- drawmap.js
+++ drawmap.js
@@ -1,5 +1,7 @@
 var imgCache = {};
 
+var bgLayers = ["water", "dirt", "grass", "desert"];
+
 function drawImg(ctx, imgName, x, y) {
     if (!imgCache[imgName]) {
         var ldImg = new Image();
@@ -21,6 +23,25 @@ function drawImgFromBottom(ctx, imgName, x, bottom) {
 function drawMap(ctx, world, xScroll, yScroll) {
     ctx.fillStyle = '#333333';
     ctx.fillRect(0, 0, 800, 600);
+    bgLayers.forEach(function(bgLayer) {
+        for (var y = -1; y < 700 / 53; y++) { for (var x = 0; x < 800 / 80; x++) {
+            var worldX = x + xScroll;
+            var worldY = y + yScroll;
+            if (worldX < 0 || worldY < 0 || worldX >= world._map[0].length || worldY >= world._map.length) {
+                continue;
+            }
+            var cell = world._map[worldY][worldX];
+            cell.forEach(function(e) {
+                var app = e.type.appearance;
+                if (app.constructor == Array && app[1] == bgLayer) {
+                    if (!e.get('imgVariant')) {
+                        e.set('imgVariant', randint(2) + 1);
+                    }
+                    drawImgFromBottom(ctx, 'bg-' + bgLayer + '-' + (e.get('imgVariant') - 1), x * 80 - 8, y * 53 + 8);
+                }
+            });
+        }}
+    });
     for (var y = -1; y < 700 / 53; y++) { for (var x = 0; x < 800 / 80; x++) {
         var worldX = x + xScroll;
         var worldY = y + yScroll;
@@ -29,9 +50,14 @@ function drawMap(ctx, world, xScroll, yScroll) {
         }
         var cell = world._map[worldY][worldX];
         cell.forEach(function(e) {
-            /*c.fillStyle = e.type.appearance;
-            c.fillRect(x * 25, y * 25, 24, 24);*/
-            drawImgFromBottom(ctx, e.type.appearance, x * 80, y * 53);
+            var app = e.type.appearance;
+            if (app.constructor == Array) {
+                if (app[0]) {
+                    drawImgFromBottom(ctx, app[0], x * 80, y * 53);
+                }
+            } else {
+                drawImgFromBottom(ctx, app, x * 80, y * 53);
+            }
         });
     }}
 }
